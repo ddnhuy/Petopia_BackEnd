3a7c704b7d3098a670031bb31d02f4d86c2b8a78
ddnhuy <doan.nhathuy.03@gmail.com>
2025-03-03 10:49:03 +0700

    Sprint 3: Hotfix: Fix errors when get comments by post id

--- src/Application/Pets/Get/GetPetsQuery.cs
+++ src/Application/Pets/Get/GetPetsQuery.cs
@@ -1,5 +1,7 @@
 ﻿using Application.Abstractions.Messaging;
+using Application.DTOs;
 using Application.DTOs.Pet;
+using Application.DTOs.Post;
 
 namespace Application.Pets.Get;
-public sealed record GetPetsQuery(string UserId, int Page, int PageSize) : IQuery<(List<PetDto>, int, int)>;
+public sealed record GetPetsQuery(string UserId, int Page, int PageSize) : IQuery<PaginatedByOffsetListDto<PetDto>>;
---
+++ src/Web.Api/Endpoints/Statics/StaticTypes.cs
@@ -0,0 +1,44 @@
+﻿using Application.DTOs;
+using Application.Statics.GetNotificationTypes;
+using Application.Statics.GetPetTypes;
+using Application.Statics.GetReactionTargetTypes;
+using Asp.Versioning;
+using Asp.Versioning.Builder;
+using MediatR;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Statics;
+
+public class StaticTypes : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app, ApiVersionSet apiVersionSet)
+    {
+        RouteGroupBuilder group = app.MapGroup("v{version:apiVersion}/statics");
+        group.WithTags(Tags.Static);
+        group.WithApiVersionSet(apiVersionSet);
+        group.MapToApiVersion(new ApiVersion(2, 0));
+
+        group.MapGet("/notification-types", async (ISender sender, CancellationToken cancellationToken) =>
+        {
+            GetNotificationTypesQuery query = new();
+            Result<List<StaticTypeDto>> result = await sender.Send(query, cancellationToken);
+            return result.Match(Results.Ok, CustomResults.Problem);
+        });
+
+        group.MapGet("/pet-types", async (ISender sender, CancellationToken cancellationToken) =>
+        {
+            GetPetTypesQuery query = new();
+            Result<List<StaticTypeDto>> result = await sender.Send(query, cancellationToken);
+            return result.Match(Results.Ok, CustomResults.Problem);
+        });
+
+        group.MapGet("/reaction-target-types", async (ISender sender, CancellationToken cancellationToken) =>
+        {
+            GetReactionTargetTypesQuery query = new();
+            Result<List<StaticTypeDto>> result = await sender.Send(query, cancellationToken);
+            return result.Match(Results.Ok, CustomResults.Problem);
+        });
+    }
+}
